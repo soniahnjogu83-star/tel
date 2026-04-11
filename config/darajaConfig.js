63243546915ecf30b0f6eb14763cb1da98103779
roswesimion-svg <roswesimion@gmail.com>
2026-04-11 14:23:14 +0300

    Create Daraja M-PESA integration configuration file

---
+++ config/darajaConfig.js
@@ -0,0 +1,12 @@
+module.exports = {
+    consumerKey: '<Your_Consumer_Key>',
+    consumerSecret: '<Your_Consumer_Secret>',
+    businessShortCode: '<Your_Business_Short_Code>',
+    tillNumber: '<Your_Till_Number>',
+    passkey: '<Your_Passkey>',
+    apiEndpoints: {
+        lipaNaMpesaOnline: 'https://api.safaricom.co.ke/mpesa/payment/request',
+        lipaNaMpesaOnlineQuery: 'https://api.safaricom.co.ke/mpesa/payment/query',
+        // Add other API endpoints as needed
+    }
+};
